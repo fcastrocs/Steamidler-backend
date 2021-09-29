f806aa8bf5dbdc31709087e3da02905f25dc671c
Francisco Castro <fjcastrocs@gmail.com>
2021-09-28 22:17:54 -0400

    added login and logout routes

--- src/routes/steamaccount.ts
+++ src/routes/steamaccount.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import * as SteamAccount from "../controllers/steamAccount";
 const router = Router();
 
-router.post("", async (req, res) => {
+router.post("add", async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
@@ -17,6 +17,40 @@ router.post("", async (req, res) => {
     return res.status(400).send(errorStr);
   }
 
+  return res.sendStatus(200);
+});
+
+router.post("login", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.sendStatus(400);
+  }
+
+  try {
+    await SteamAccount.login(req.session.userId, username);
+  } catch (error) {
+    const errorStr = normalizeError(error);
+    return res.status(400).send(errorStr);
+  }
+  return res.sendStatus(200);
+});
+
+router.post("logout", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.sendStatus(400);
+  }
+
+  try {
+    await SteamAccount.logout(req.session.userId, username);
+  } catch (error) {
+    const errorStr = normalizeError(error);
+    return res.status(400).send(errorStr);
+  }
   return res.sendStatus(200);
 });
 
